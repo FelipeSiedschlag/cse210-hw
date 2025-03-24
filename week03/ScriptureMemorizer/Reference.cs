44e895dfd2b65cd15a933a3ea67ac0714b73c532
FelipeSiedschlag <fsiedsch@byupathway.edu>
2025-03-24 19:37:41 -0300

    Word class complete

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,20 @@
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+
+    public Reference(string book, in chapter, int verse) {
+
+    }
+
+    public Reference(string book, in chapter, int startVerse, int endVerse) {
+
+    }
+
+    public string GetDisplayText() {
+        return 
+    }
+}
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Words> _words;
+
+    public Scripture(Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRamdomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        
+    }
+}
