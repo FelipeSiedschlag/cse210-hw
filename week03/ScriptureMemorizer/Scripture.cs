a252bb009acb2f1b4ad7e582b56ac1efb119025c
FelipeSiedschlag <fsiedsch@byupathway.edu>
2025-03-25 20:33:33 -0300

    Finish Encapsulation Program

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,16 +1,31 @@
+using Microsoft.VisualBasic;
+
 public class Scripture
 {
     private Reference _reference;
-    private List<Words> _words;
+    private List<Word> _words;
 
-    public Scripture(Reference Reference, string text)
+    public Scripture(Reference reference, string text)
     {
-
+        _reference = reference;
+        _words = new List<Word>();
     }
 
     public void HideRamdomWords(int numberToHide)
     {
+        Random random = new Random();
+        int count = 0;
 
+        while (count < numberToHide)
+        {
+            int index = random.Next(_words.Count); // generate a random index
+            Word word = _words[index];
+            if (!word.IsHidden())
+            {
+                word.Hide();
+                count++;
+            }
+        }
     }
 
     public string GetDisplayText()
